0e3d0bfb516039f53ff54bcb5db52633a5b416a9
Tetiana Bondarenko <tn.a.bondarenko@gmail.com>
2020-12-03 22:59:14 +0200

    fixed bugs added sounds folder

--- my_javascript.js
+++ my_javascript.js
@@ -1,23 +1,28 @@
-function openMonth(evt, monthName) {
-        // Объявить все переменные
-        var i, content, month;
+let i = 0;
+let content = document.getElementsByClassName("content");
+let month = document.getElementsByClassName("month");
+let img = ["images/jan.jpg","images/feb.jpg","images/apr.jpg",
+    "images/may.jpg", "images/june.jpg", "images/july.jpg",
+    "images/apr.jpg", "images/apr.jpg", "images/apr.jpg",
+    "images/apr.jpg", "images/apr.jpg", "images/apr.jpg"];
 
-        // Получить все элементы с class="content" и скрыть их
-        content = document.getElementsByClassName("content");
+function openMonth(evt, monthName) {
         for (i = 0; i < content.length; i++) {
             content[i].style.display = "none";
         }
-
-        // Получить все элементы с class="month" и удалить class "active"
-        month = document.getElementsByClassName("month");
         for (i = 0; i < month.length; i++) {
             month[i].className = month[i].className.replace(" active", "");
         }
-
-        // Показать текущую вкладку и добавить "active" класс по ссылке, открывшей вкладку
         document.getElementById(monthName).style.display = "block";
         evt.currentTarget.className += " active";
-
-    
 }
+
+window.addEventListener("load", addImg);
+function addImg (){
+    for (let i = 0; i < content.length; i++) {    
+        content[i].innerHTML += '<img src="' + img[i] + '">';
+        content[i].innerHTML;       
+    }
+}
+
 
