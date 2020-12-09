b6883de5d13fe07a5f564ca9a9142fc2aa8bebcc
Tetiana Bondarenko <tn.a.bondarenko@gmail.com>
2020-12-09 15:41:15 +0200

    fixed initApp

--- my_javascript.js
+++ my_javascript.js
@@ -10,18 +10,17 @@ const soundsPaths = ["sounds/jan.mp3", "sounds/feb.mp3", "sounds/mar.mp3",
 "sounds/jul.mp3","sounds/aug.mp3","sounds/sep.mp3",
     "sounds/oct.mp3", "sounds/nov.mp3", "sounds/dec.mp3"];    
 
-window.onload = function () {
-    content[0].style.display = "block";
-    document.querySelector('.nav').addEventListener('click', showMonth);
-    function showMonth(event) {
-        if (event.target.className == 'month') {
-            const dataMonth = event.target.getAttribute('data-month');
-            for (let i = 0; i < month.length; i++){
-                month[i].classList.remove('active');
-            }
-            event.target.classList.add('active');
+window.addEventListener("load", showMonth);
+window.addEventListener("load", initApp);
+
+
+    function showMonth({target}) {
+        if (target.classList.contains('month')) {
+            document.querySelector('.month.active').classList.remove('active');
+            const dataMonth = target.getAttribute('data-month');
+            target.classList.add('active');
             for (let i = 0; i < content.length; i++){
-                if (dataMonth == i) {
+                if (parseInt(dataMonth) === i) {
                     content[i].style.display = "block";   
                 } else {
                     content[i].style.display = "none";  
@@ -30,17 +29,15 @@ window.onload = function () {
             }
         }
     }
-}
 
-window.addEventListener("load", addContent);
-function addContent (){
+function addContent() {
     for (let i = 0; i < content.length; i++) {    
-        content[i].innerHTML += '<img class="content_img" src="' + imagesPaths
-    [i] + '" alt="Nice picture of nature">';
+        content[i].innerHTML += `<img class="content_img" src="${imagesPaths
+    [i]}" alt="Nice picture of nature">`;
         content[i].innerHTML;    
         content[i].insertAdjacentHTML("afterbegin", "<audio controls><audio>");
         audTag = document.querySelectorAll("audio");
-        audTag[i].innerHTML = '<source src="' + soundsPaths[i] + ' "type="audio/mp3", controls="controls">';
+        audTag[i].innerHTML = `<source src="${soundsPaths[i]}" "type="audio/mp3"controls="controls">`;
         audTag[i].innerHTML += "Your browser does not support the audio element.";
     }
 };
@@ -48,3 +45,14 @@ function addContent (){
 function pauseAudio(i) {
     audTag[i].pause();
 } 
+
+function stayActive() {
+    content[0].style.display = "block";
+    month[0].classList.add('active');
+    document.querySelector('.nav').addEventListener('click',showMonth);
+}
+
+function initApp() {
+    addContent();
+    stayActive();
+}
